7738bed109c2302b7d7f52b9aaf92ff852576210
Sumbal Khan <60316112+sumbal28@users.noreply.github.com>
2023-03-06 01:47:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,125 +1,142 @@
-// Array of special characters to be included in password
-var specialCharacters = [
-  "@",
-  "%",
-  "+",
-  "\\",
-  "/",
-  "'",
-  "!",
-  "#",
-  "$",
-  "^",
-  "?",
-  ":",
-  ",",
-  ")",
-  "(",
-  "}",
-  "{",
-  "]",
-  "[",
-  "~",
-  "-",
-  "_",
-  ".",
-];
+let generateBtn = document.querySelector("#generate");
 
-// Array of numeric characters to be included in password
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var builtArray = [];
-
-// Function to prompt user for password options
-function getPasswordOptions() {
-  bigArray = bigArray.concat(lowerCasedCharacters);
-}
-//getPasswordOptions();
-
-// Function for getting a random element from an array
-function getRandom(arr) {}
-
-// Function to generate password with user input
 function generatePassword() {
-  let practicePassword = "";
+  // Array of uppercase characters to be included in password
+  let uppercaseArray = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
 
-  let randomIndex = Math.floor(Math.random() * specialCharacters.length);
+  // Array of lowercase characters to be included in password
 
-  for (let i = 0; i < 60; i++) {
-    practicePassword += "j";
+  let lowercaseArray = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  // Array of numeric characters to be included in password
+  let numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  // Array of special characters to be included in password
+  let characterArray = [
+    "@",
+    "%",
+    "+",
+    "/",
+    "'",
+    "!",
+    "#",
+    "$",
+    "^",
+    "?",
+    ":",
+    ",",
+    ")",
+    "(",
+    "}",
+    "{",
+    "]",
+    "[",
+    "~",
+    "-",
+    "_",
+    ".",
+  ];
+
+  let resultArray = [];
+  let userArray = [];
+
+  uppercaseArray[1];
+
+  // Creating variables to prompt user for password options
+  let numCharacter = prompt("How many number of character? between 7 to 100");
+  let numbers = confirm("want numbers in password?");
+  let uppercases = confirm("Want uppercases in password?");
+  let lowercases = confirm("Want lowercases in password?");
+  let characters = confirm("Want special characters in password?");
+
+  if (numbers) {
+    resultArray = resultArray.concat(numbArray);
   }
-  return lowerCasedCharacters + numericCharacters + upperCasedCharacters;
-}
 
+  if (uppercases) {
+    resultArray = resultArray.concat(uppercaseArray);
+  }
+
+  if (lowercases) {
+    resultArray = resultArray.concat(lowercaseArray);
+  }
+
+  if (characters) {
+    resultArray = resultArray.concat(characterArray);
+  }
+
+  console.log(resultArray);
+
+  for (let i = 0; i < numCharacter; i++) {
+    userArray.push(resultArray[Math.floor(Math.random() * resultArray.length)]);
+  }
+  return userArray.join("");
+}
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
+
+function copyToClipboard() {}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
